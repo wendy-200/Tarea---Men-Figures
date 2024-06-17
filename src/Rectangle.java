0478a6fd2c1cb869c69c5ff3debed8416880ae6b
wendy <Wortizm2@est.ups.edu.ec>
2024-06-16 19:16:31 -0500

    Update Rectangle.java

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Rectangle {
+public class Rectangulo {
+    public static void main(String[] args) {
+        printRectangle(3,7);
+    }
+    public static void printRectangle(int altura, int ancho) {
+        for (int i = 1; i <= altura; i++) {
+            for (int j = 1; j <ancho ; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
 }
