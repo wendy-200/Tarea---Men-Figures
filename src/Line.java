e1ca6bbc3257693d2b3c7706375642229a067c15
wendy <Wortizm2@est.ups.edu.ec>
2024-06-16 19:15:15 -0500

    Update Line.java

--- src/Line.java
+++ src/Line.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Line {
+public class Line {
+    private int length;
+
+    public Line() {
+    }
+
+    public Line(int length) {
+        this.length = length;
+    }
+
+    public void printLine(){
+        System.out.println("----------------- Printing Line (" +  length  +")----------------");
+        System.out.println(" ");
+        for(int i = 0; i < length; i++) {
+            System.out.print("* ");
+        }
+        System.out.println(" ");
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
 }
