35ef4fe5f3a767de0b2902a207fb3f673d77f3e4
wendy <Wortizm2@est.ups.edu.ec>
2024-06-16 19:26:17 -0500

    Update Rectangle.java

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,10 +1,14 @@
-public class Rectangulo {
+public class Rectangle {
     public static void main(String[] args) {
-        printRectangle(3,7);
+        System.out.println("Rectángulo de alto 3 y ancho 4:");
+        printRectangle(3, 4);
+        System.out.println("\nRectángulo de alto 5 y ancho 6:");
+        printRectangle(5, 6);
     }
-    public static void printRectangle(int altura, int ancho) {
-        for (int i = 1; i <= altura; i++) {
-            for (int j = 1; j <ancho ; j++) {
+
+    public static void printRectangle(int height, int width) {
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= width; j++) {
                 System.out.print("*");
             }
             System.out.print("\n");
