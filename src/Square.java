146748458b871217196808208e1074f2035fd23a
wendy <Wortizm2@est.ups.edu.ec>
2024-06-16 19:26:41 -0500

    Update Square.java

--- src/Square.java
+++ src/Square.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Square {
+public class Square {
+    public static void printSquare(int Size) {
+        for (int i = 1; i < Size; i++) {
+            for (int j = 1; j <Size; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
 }
